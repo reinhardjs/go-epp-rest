09a8523d01d1d068bc31c226044f0884ef6cab86
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-15 09:13:28 +0700

    chore(go-epp-rest): Init

---
+++ go.mod
@@ -0,0 +1,15 @@
+module gitlab.com/reinhardjs/verisign-epp-rest
+
+go 1.19
+
+require github.com/bombsimon/epp-go v0.0.0-20190414193720-3d0e729e1869
+
+require (
+	aqwari.net/xml v0.0.0-20190411173135-9e2dd5ec99d1 // indirect
+	github.com/google/uuid v1.1.1 // indirect
+	github.com/lestrrat-go/libxml2 v0.0.0-20201123224832-e6d9de61b80d // indirect
+	github.com/pkg/errors v0.8.1 // indirect
+	golang.org/x/net v0.0.0-20190404232315-eb5bcb51f2a3 // indirect
+	golang.org/x/text v0.3.0 // indirect
+	gopkg.in/xmlpath.v1 v1.0.0-20140413065638-a146725ea6e7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"log"
+	"math/big"
+	"os"
+	"time"
+
+	epp "github.com/bombsimon/epp-go"
+)
+
+func main() {
+	client := &epp.Client{
+		TLSConfig: &tls.Config{
+			InsecureSkipVerify: true,
+			Certificates:       []tls.Certificate{generateCertificate()},
+		},
+	}
+
+	greeting, err := client.Connect(":4701")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
+	fmt.Println(string(greeting))
+
+	fmt.Println("---- Enter EPP frame terminated by a double newline ----")
+
+	fmt.Println("> Automatic login!")
+	time.Sleep(1 * time.Second)
+
+	response, err := client.Login("some-user", "some-password")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	fmt.Println(string(response))
+}
+
+func generateCertificate() tls.Certificate {
+	key, _ := rsa.GenerateKey(rand.Reader, 2048)
+
+	cert := &x509.Certificate{
+		SerialNumber: big.NewInt(1653),
+		Subject: pkix.Name{
+			CommonName:   "epp.example.test",
+			Organization: []string{"Simple Server Test"},
+			Country:      []string{"SE"},
+			Locality:     []string{"Stockholm"},
+		},
+		NotBefore:             time.Now(),
+		NotAfter:              time.Now().AddDate(0, 0, 1),
+		IsCA:                  true,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth, x509.ExtKeyUsageServerAuth},
+		KeyUsage:              x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+		BasicConstraintsValid: true,
+	}
+
+	certificate, _ := x509.CreateCertificate(rand.Reader, cert, cert, key.Public(), key)
+
+	return tls.Certificate{
+		Certificate: [][]byte{certificate},
+		PrivateKey:  key,
+	}
+}
