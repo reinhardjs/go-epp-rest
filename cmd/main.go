3d1d97657e5756151279ab8824ce664abd592cf8
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-16 15:36:03 +0700

    chore(go-epp-rest): refactor main.go

---
+++ cmd/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/pkg/errors"
+	"gitlab.com/reinhardjs/go-epp-rest/internal/config"
+	"gitlab.com/reinhardjs/go-epp-rest/internal/server"
+)
+
+func main() {
+	cfg, err := config.InitConfig()
+
+	if err != nil {
+		log.Println(errors.Wrap(err, "main: init config:"))
+	}
+
+	s := server.NewServer(cfg)
+	err = s.Run()
+
+	if err != nil {
+		log.Println(errors.Wrap(err, "main: new server:"))
+	}
+}
