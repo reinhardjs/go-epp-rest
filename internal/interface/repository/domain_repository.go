80bcdb7e0a7f4a618c9991e08eaac87ae53b3f2f
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-20 17:09:27 +0700

    feat(check-domain-host-contact): implement DoQueryDomain details

--- internal/interface/repository/domain_repository.go
+++ internal/interface/repository/domain_repository.go
@@ -1,16 +1,36 @@
 package repository
 
 import (
+	"github.com/bombsimon/epp-go"
+	"github.com/bombsimon/epp-go/types"
+	"gitlab.com/merekmu/go-epp-rest/internal/infrastructure"
 	"gitlab.com/merekmu/go-epp-rest/internal/usecase/repository"
 )
 
 type domainRepository struct {
+	eppClient infrastructure.EppClient
 }
 
-func NewDomainRepository() repository.DomainRepository {
-	return &domainRepository{}
+func NewDomainRepository(eppClient infrastructure.EppClient) repository.DomainRepository {
+	return &domainRepository{eppClient}
 }
 
 func (r *domainRepository) DoQueryDomain(domainList []string, ext string, langTag string) (string, error) {
-	return "", nil
+	domainCheck := types.DomainCheckType{
+		Check: types.DomainCheck{
+			Names: domainList,
+		},
+	}
+
+	encoded, err := epp.Encode(domainCheck, epp.ClientXMLAttributes())
+	if err != nil {
+		return "", err
+	}
+
+	byteResponse, err := r.eppClient.Send(encoded)
+	if err != nil {
+		return "", err
+	}
+
+	return string(byteResponse), nil
 }
