f6a0f8e9ffd5c62ca20dd58e841fc539302ab0b2
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-21 16:10:32 +0700

    feat(check-domain-host-contact): reuse generic with constraint

--- internal/usecase/presenter/registrar_presenter.go
+++ internal/usecase/presenter/registrar_presenter.go
@@ -1,5 +1,7 @@
 package presenter
 
-type RegistrarPresenter interface {
-	ResponseCheck(response []byte) (string, error)
+import "gitlab.com/merekmu/go-epp-rest/internal/interface/constraints"
+
+type RegistrarPresenter[T constraints.RegistrarResponseConstraint] interface {
+	ResponseCheck(response []byte) (T, error)
 }
