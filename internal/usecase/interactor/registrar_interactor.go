61cd01a6bb34f817bbba73dd1fb18db9754d0fda
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-21 14:58:53 +0700

    feat(check-domain-host-contact): refactor registrar interactor

--- internal/usecase/interactor/registrar_interactor.go
+++ internal/usecase/interactor/registrar_interactor.go
@@ -2,27 +2,28 @@ package interactor
 
 import (
 	"github.com/pkg/errors"
+	"gitlab.com/merekmu/go-epp-rest/internal/interface/constraints"
 	"gitlab.com/merekmu/go-epp-rest/internal/usecase/presenter"
 	"gitlab.com/merekmu/go-epp-rest/internal/usecase/repository"
 )
 
-type registrarInteractor struct {
+type registrarInteractor[T constraints.RegistrarResponseConstraint] struct {
 	RegistrarRepository repository.RegistrarRepository
-	RegistrarPresenter  presenter.RegistrarPresenter
+	RegistrarPresenter  presenter.RegistrarPresenter[T]
 }
 
 type RegistrarInteractor interface {
 	Check(data interface{}, ext string, langTag string) (string, error)
 }
 
-func NewDomainInteractor(registrarRepository repository.RegistrarRepository, registrarPresenter presenter.RegistrarPresenter) RegistrarInteractor {
-	return &registrarInteractor{
+func NewRegistrarInteractor[T constraints.RegistrarResponseConstraint](registrarRepository repository.RegistrarRepository, registrarPresenter presenter.RegistrarPresenter[T]) RegistrarInteractor {
+	return &registrarInteractor[T]{
 		RegistrarRepository: registrarRepository,
 		RegistrarPresenter:  registrarPresenter,
 	}
 }
 
-func (interactor *registrarInteractor) Check(data interface{}, ext string, langTag string) (string, error) {
+func (interactor *registrarInteractor[T]) Check(data interface{}, ext string, langTag string) (string, error) {
 	response, err := interactor.RegistrarRepository.Check(data)
 
 	if err != nil {
