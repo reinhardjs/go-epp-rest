69de26e60a58827c8de169deb950e618a1b85ae8
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-21 10:46:22 +0700

    eat(check-domain-host-contact): added registry and domain registry

---
+++ internal/registry/registry.go
@@ -0,0 +1,24 @@
+package registry
+
+import (
+	"gitlab.com/merekmu/go-epp-rest/internal/infrastructure"
+	"gitlab.com/merekmu/go-epp-rest/internal/interface/controller"
+)
+
+type registry struct {
+	eppClient infrastructure.EppClient
+}
+
+type Registry interface {
+	NewAppController() controller.AppController
+}
+
+func NewRegistry(eppClient infrastructure.EppClient) Registry {
+	return &registry{eppClient: eppClient}
+}
+
+func (r *registry) NewAppController() controller.AppController {
+	return controller.AppController{
+		Domain: r.NewDomainController(),
+	}
+}
