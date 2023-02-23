55d3f6cce9226316eba4f75013982a75da5a639c
reinhardjs <reinhardjsilalahi@gmail.com>
2023-02-23 15:06:28 +0700

    feat(info-domain-host-contact): #MER-11 added transfer epp type

---
+++ pkg/registry_epp/types/transfer.go
@@ -0,0 +1,25 @@
+package types
+
+// TransferType represents a transfer check command.
+type TransferType struct {
+	TransferParent Transfer `xml:"command>transfer"`
+}
+
+type Transfer struct {
+	Operation string         `xml:"op,attr"`
+	Detail    TransferDetail `xml:"urn:ietf:params:xml:ns:domain-1.0 transfer"`
+}
+
+// TransferDetail represents a transfer check request to the EPP server.
+type TransferDetail struct {
+	Name     string    `xml:"name"`
+	AuthInfo *AuthInfo `xml:"authInfo,omitempty"`
+}
+
+// TransferCheckData represents the response for a transfer check command.
+type TransferCheckData struct {
+	Name           string `xml:"name"`
+	TransferStatus string `xml:"trStatus"`
+	ReID           string `xml:"reId"`
+	ReDate         string `xml:"reDate"`
+}
