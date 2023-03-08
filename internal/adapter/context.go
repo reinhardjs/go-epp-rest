aa05acb50e5bb13adcd824d0788643a07a01c3c2
reinhardjs <reinhardjsilalahi@gmail.com>
2023-03-08 17:18:24 +0700

    feat(error-handling): #MER-18 moved Context adapter into adapter package

--- internal/adapter/context.go
+++ internal/adapter/context.go
@@ -1,4 +1,4 @@
-package wrapper
+package adapter
 
 import "github.com/gin-gonic/gin"
 
--- internal/delivery/http/router/router.go
+++ internal/delivery/http/router/router.go
@@ -2,45 +2,45 @@ package router
 
 import (
 	"github.com/gin-gonic/gin"
+	"gitlab.com/merekmu/go-epp-rest/internal/adapter"
 	"gitlab.com/merekmu/go-epp-rest/internal/delivery/http/controllers"
-	"gitlab.com/merekmu/go-epp-rest/internal/infrastructure/wrapper"
 )
 
 func NewRouter(appController controllers.AppController) *gin.Engine {
 	router := gin.Default()
 
-	router.GET("/domain/check", func(c *gin.Context) { appController.Domain.Check(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/create", func(c *gin.Context) { appController.Domain.Create(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/delete", func(c *gin.Context) { appController.Domain.Delete(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/info", func(c *gin.Context) { appController.Domain.Info(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/secdnsupdate", func(c *gin.Context) { appController.Domain.SecDNSUpdate(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/contact/update", func(c *gin.Context) { appController.Domain.ContactUpdate(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/status/update", func(c *gin.Context) { appController.Domain.StatusUpdate(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/authinfo/update", func(c *gin.Context) { appController.Domain.AuthInfoUpdate(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/nameserver/update", func(c *gin.Context) { appController.Domain.NameserverUpdate(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/domain/renew", func(c *gin.Context) { appController.Domain.Renew(&wrapper.ContextAdapter{Context: c}) })
+	router.GET("/domain/check", func(c *gin.Context) { appController.Domain.Check(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/create", func(c *gin.Context) { appController.Domain.Create(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/delete", func(c *gin.Context) { appController.Domain.Delete(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/info", func(c *gin.Context) { appController.Domain.Info(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/secdnsupdate", func(c *gin.Context) { appController.Domain.SecDNSUpdate(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/contact/update", func(c *gin.Context) { appController.Domain.ContactUpdate(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/status/update", func(c *gin.Context) { appController.Domain.StatusUpdate(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/authinfo/update", func(c *gin.Context) { appController.Domain.AuthInfoUpdate(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/nameserver/update", func(c *gin.Context) { appController.Domain.NameserverUpdate(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/domain/renew", func(c *gin.Context) { appController.Domain.Renew(&adapter.ContextAdapter{Context: c}) })
 
-	router.GET("/host/check", func(c *gin.Context) { appController.Host.Check(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/create", func(c *gin.Context) { appController.Host.Create(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/update", func(c *gin.Context) { appController.Host.Update(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/delete", func(c *gin.Context) { appController.Host.Delete(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/info", func(c *gin.Context) { appController.Host.Info(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/change", func(c *gin.Context) { appController.Host.Change(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/host/checkcreate", func(c *gin.Context) { appController.Host.CheckAndCreate(&wrapper.ContextAdapter{Context: c}) })
+	router.GET("/host/check", func(c *gin.Context) { appController.Host.Check(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/create", func(c *gin.Context) { appController.Host.Create(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/update", func(c *gin.Context) { appController.Host.Update(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/delete", func(c *gin.Context) { appController.Host.Delete(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/info", func(c *gin.Context) { appController.Host.Info(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/change", func(c *gin.Context) { appController.Host.Change(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/host/checkcreate", func(c *gin.Context) { appController.Host.CheckAndCreate(&adapter.ContextAdapter{Context: c}) })
 
-	router.GET("/contact/check", func(c *gin.Context) { appController.Contact.Check(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/contact/create", func(c *gin.Context) { appController.Contact.Create(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/contact/update", func(c *gin.Context) { appController.Contact.Update(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/contact/delete", func(c *gin.Context) { appController.Contact.Delete(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/contact/info", func(c *gin.Context) { appController.Contact.Info(&wrapper.ContextAdapter{Context: c}) })
+	router.GET("/contact/check", func(c *gin.Context) { appController.Contact.Check(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/contact/create", func(c *gin.Context) { appController.Contact.Create(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/contact/update", func(c *gin.Context) { appController.Contact.Update(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/contact/delete", func(c *gin.Context) { appController.Contact.Delete(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/contact/info", func(c *gin.Context) { appController.Contact.Info(&adapter.ContextAdapter{Context: c}) })
 
-	router.GET("/transfer/check", func(c *gin.Context) { appController.Transfer.Check(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/transfer/request", func(c *gin.Context) { appController.Transfer.Request(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/transfer/cancel", func(c *gin.Context) { appController.Transfer.Cancel(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/transfer/approve", func(c *gin.Context) { appController.Transfer.Approve(&wrapper.ContextAdapter{Context: c}) })
-	router.GET("/transfer/reject", func(c *gin.Context) { appController.Transfer.Approve(&wrapper.ContextAdapter{Context: c}) })
+	router.GET("/transfer/check", func(c *gin.Context) { appController.Transfer.Check(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/transfer/request", func(c *gin.Context) { appController.Transfer.Request(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/transfer/cancel", func(c *gin.Context) { appController.Transfer.Cancel(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/transfer/approve", func(c *gin.Context) { appController.Transfer.Approve(&adapter.ContextAdapter{Context: c}) })
+	router.GET("/transfer/reject", func(c *gin.Context) { appController.Transfer.Approve(&adapter.ContextAdapter{Context: c}) })
 
-	router.GET("/poll", func(c *gin.Context) { appController.Poll.Poll(&wrapper.ContextAdapter{Context: c}) })
+	router.GET("/poll", func(c *gin.Context) { appController.Poll.Poll(&adapter.ContextAdapter{Context: c}) })
 
 	return router
 }
