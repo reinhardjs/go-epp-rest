0b51b87e993ba2ad97dc6751d880b9706e00e210
reinhardjs <reinhardjsilalahi@gmail.com>
2023-03-09 08:18:40 +0700

    feat(error-handling): #MER-18 refactor domain presenter

--- internal/usecase/presenter/domain_presenter.go
+++ internal/usecase/presenter/domain_presenter.go
@@ -6,14 +6,14 @@ import (
 )
 
 type DomainPresenter interface {
-	CheckSuccess(ctx infrastructure.Context, obj response.CheckDomainResponse)
-	CreateSuccess(ctx infrastructure.Context, obj response.CreateDomainResponse)
-	DeleteSuccess(ctx infrastructure.Context, obj response.DeleteDomainResponse)
-	InfoSuccess(ctx infrastructure.Context, obj response.InfoDomainResponse)
-	SecDNSUpdateSuccess(ctx infrastructure.Context, obj response.SecDNSUpdateResponse)
-	ContactUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse)
-	StatusUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse)
-	AuthInfoUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse)
-	NameserverUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse)
-	RenewSuccess(ctx infrastructure.Context, obj response.RenewDomainResponse)
+	CheckSuccess(ctx infrastructure.Context, obj response.CheckDomainResponse) error
+	CreateSuccess(ctx infrastructure.Context, obj response.CreateDomainResponse) error
+	DeleteSuccess(ctx infrastructure.Context, obj response.DeleteDomainResponse) error
+	InfoSuccess(ctx infrastructure.Context, obj response.InfoDomainResponse) error
+	SecDNSUpdateSuccess(ctx infrastructure.Context, obj response.SecDNSUpdateResponse) error
+	ContactUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse) error
+	StatusUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse) error
+	AuthInfoUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse) error
+	NameserverUpdateSuccess(ctx infrastructure.Context, obj response.DomainUpdateResponse) error
+	RenewSuccess(ctx infrastructure.Context, obj response.RenewDomainResponse) error
 }
