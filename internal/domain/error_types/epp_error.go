b65b6986722a565b1c3970cf858f13abadd97d33
reinhardjs <reinhardjsilalahi@gmail.com>
2023-03-09 07:27:10 +0700

    feat(error-handling): #MER-18 add error type EPPCommandError

--- internal/domain/error_types/epp_error.go
+++ internal/domain/error_types/epp_error.go
@@ -1,7 +1,21 @@
 package error_types
 
+import (
+	"fmt"
+
+	"gitlab.com/merekmu/go-epp-rest/internal/domain/dto/response"
+)
+
 type HostNameNotAvailableError struct{}
 
 func (e *HostNameNotAvailableError) Error() string {
 	return "host name not available"
+}
+
+type EPPCommandError struct {
+	Result response.Result
+}
+
+func (e *EPPCommandError) Error() string {
+	return fmt.Sprintf("%v", e.Result)
 }
