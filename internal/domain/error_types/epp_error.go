fd4db83025c43a229c42b614a5eda2293d6cb0e0
reinhardjs <reinhardjsilalahi@gmail.com>
2023-03-10 17:27:20 +0700

    feat(error-handling): #MER-18 refactor epp error type

--- internal/domain/error_types/epp_error.go
+++ internal/domain/error_types/epp_error.go
@@ -6,12 +6,6 @@ import (
 	"gitlab.com/merekmu/go-epp-rest/internal/domain/dto/response"
 )
 
-type HostNameNotAvailableError struct{}
-
-func (e *HostNameNotAvailableError) Error() string {
-	return "host name not available"
-}
-
 type EPPCommandError struct {
 	Result response.Result
 }
