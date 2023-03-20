d11754208e5764d6d6d3569a683b7c9efd5bed78
reinhardjs <reinhardjsilalahi@gmail.com>
2023-03-20 13:55:30 +0700

    feat(session-pool): #no-jira periodic free os memory

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"log"
+	"runtime/debug"
+	"time"
 
 	"github.com/pkg/errors"
 	"gitlab.com/merekmu/go-epp-rest/config"
 	"gitlab.com/merekmu/go-epp-rest/internal/server"
 )
 
+func periodicFree(d time.Duration) {
+	tick := time.Tick(d)
+	for _ = range tick {
+		debug.FreeOSMemory()
+	}
+}
+
 func main() {
+	go periodicFree(1 * time.Minute)
+
 	cfg, err := config.InitConfig()
 
 	if err != nil {
