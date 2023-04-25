6a8044df97103daa8207d4ff6b4b8f10dd67c65f
reinhardjs <reinhardjsilalahi@gmail.com>
2023-04-25 16:07:22 +0700

    feat(session-pool): #no-jira removed InitLogin method

--- internal/usecase/adapter/epp_client.go
+++ internal/usecase/adapter/epp_client.go
@@ -4,7 +4,6 @@ import "net"
 
 type EppClient interface {
 	Send(data []byte) ([]byte, error)
-	InitLogin(username, password string) ([]byte, error)
 	DoLogin(conn net.Conn) ([]byte, error)
 	SendHello(conn net.Conn) ([]byte, error)
 }
