a257c84410200c2e09db66c94903d9481bc2d2ae
reinhardjs <reinhardjsilalahi@gmail.com>
2023-05-03 11:04:00 +0700

    feat(session-pool): #no-jira disable logrus

--- internal/utils/logger.go
+++ internal/utils/logger.go
@@ -1,14 +1,5 @@
 package utils
 
-import (
-	"fmt"
-	"io"
-	"os"
-	"strings"
-
-	"github.com/sirupsen/logrus"
-)
-
 var instance *logger
 
 type logger struct {
@@ -34,27 +25,27 @@ func GetLoggerInstance() Logger {
 }
 
 func (l *logger) Run() {
-	// Create a file for writing logs
-	file, err := os.OpenFile("logs/api.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
-	if err != nil {
-		panic(err)
-	}
+	// // Create a file for writing logs
+	// file, err := os.OpenFile("logs/api.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	// Set the logger to write output to both the file and terminal
-	logrus.SetOutput(io.MultiWriter(os.Stdout, file))
+	// // Set the logger to write output to both the file and terminal
+	// logrus.SetOutput(io.MultiWriter(os.Stdout, file))
 
-	logrus.SetFormatter(&logrus.TextFormatter{
-		DisableQuote:    true,
-		TimestampFormat: "2006-01-02 15:04:05.999999999",
-	})
+	// logrus.SetFormatter(&logrus.TextFormatter{
+	// 	DisableQuote:    true,
+	// 	TimestampFormat: "2006-01-02 15:04:05.999999999",
+	// })
 
-	go func() {
-		for msg := range l.logChannel {
-			logrus.Info(msg)
-		}
-	}()
+	// go func() {
+	// 	for msg := range l.logChannel {
+	// 		logrus.Info(msg)
+	// 	}
+	// }()
 }
 
 func (l *logger) Info(args ...interface{}) {
-	l.logChannel <- strings.Trim(fmt.Sprintf("%v", args), "[]")
+	// l.logChannel <- strings.Trim(fmt.Sprintf("%v", args), "[]")
 }
